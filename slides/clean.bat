4ccd135b454343417222f5c5bace102d1acdd212
BuhDuy <nbduy23@clc.fitus.edu.vn>
2026-02-13 16:34:47 +0700

    update clean.bat

--- slides/clean.bat
+++ slides/clean.bat
@@ -1,3 +1,3 @@
 @echo off
-del /Q *.aux *.log *.nav *.out *.snm *.toc *.vrb *.fls *.fdb_latexmk *.synctex.gz 2>nul
+del /Q *.aux *.log *.nav *.out *.snm *.toc *.vrb *.fls *.fdb_latexmk *.synctex.gz *.bbl *.bcf *.run.xml 2>nul
 echo LaTeX auxiliary files cleaned.
